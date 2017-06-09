ee4df1f31ff5834412a35776a5011b2d0a416549
lisawilliams <lisa@placeblogger.com>
2017-06-09 12:32:46 -0400

    Created migration to add user_id to chores db table
    Also updated models:
    
    users have_many chores
    chores belong_to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170608035932) do
+ActiveRecord::Schema.define(version: 20170609161055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20170608035932) do
     t.string   "day_of_week"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_chores_on_user_id", using: :btree
   end
 
   create_table "examples", force: :cascade do |t|
@@ -48,5 +50,6 @@ ActiveRecord::Schema.define(version: 20170608035932) do
     t.index ["token"], name: "index_users_on_token", unique: true, using: :btree
   end
 
+  add_foreign_key "chores", "users"
   add_foreign_key "examples", "users"
 end
--- scaffolds.md
+++ scaffolds.md
@@ -1,26 +1,13 @@
-Scaffold for 'mom'
 
-```bin/rails generate scaffold mom given_name:string family_name:string```
-
-
-DONE Scaffold for 'kid'
-
-```bin/rails generate scaffold kid given_name:string family_name:string```
 
 DONE Scaffold for 'chore'
 
 ```bin/rails generate scaffold chore chore_name:string directions:string day_of_week:string```
 
-Scaffold for Add Mom ID to Kid
+Scaffold for 'users-have-chores'
 
-```rails generate migration AddMomIDtoKid```
+`rails generate migration AddUserToChore user:references`
 
-See the [following Stack Overflow thread](https://stackoverflow.com/questions/8555082/rails-how-to-setup-a-one-to-many-relationship) for how to change the models
-
-
-Scaffold for 'family'
-
-```bin/rails generate scaffold family mom:references kid:references```
-
-
-Scaffold for 'kids-have-chores'
+In models, update:
+Users have_many chores
+chores belong_to users
